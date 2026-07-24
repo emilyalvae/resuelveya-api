055204b1830bc2d4ca51431bf9681b16b2c0f9b1
emilyalvae <emilisa23@gmail.com>
2026-07-23 21:30:46 -0500

    second commit

---
+++ src/main/java/com/resuelveya/resuelve_api/ResuelveApiApplication.java
@@ -0,0 +1,13 @@
+package com.resuelveya.resuelve_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResuelveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResuelveApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/resuelveya/resuelve_api/entity/Usuario.java
@@ -0,0 +1,4 @@
+package com.resuelveya.resuelve_api.entity;
+
+public class Usuario {
+}
